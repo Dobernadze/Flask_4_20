c1da13212d26a622c3416ac8168a86d2d27b3389
Dobernadze <tima7723@gmail.com>
2022-04-14 09:46:37 +0600

    stable point

---
+++ market/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '29d090a1572a0ba21bc11f76'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+from market import routes 
