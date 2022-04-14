33d97e6c96afab66403c492b5e30d553a259719e
Dobernadze <tima7723@gmail.com>
2022-04-14 15:14:52 +0600

    end part 13 Flask_tutorial, do part 14

--- market/__init__.py
+++ market/__init__.py
@@ -9,4 +9,6 @@ app.config['SECRET_KEY'] = '29d090a1572a0ba21bc11f76'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from market import routes 
